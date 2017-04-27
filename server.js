60eaab03ee66771be737922756b68a52aa815838
Indra Bayu K <indra.b@skyshi.com>
2017-04-27 14:12:58 +0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _= require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,12 +21,13 @@ app.get('/todos', function (req, res){
 // GET /todos/:id()
 app.get('/todos/:id', function (req, res){
 	var id = parseInt(req.params.id);
-	var matchedTodo;
-	todos.forEach(function (todo){
-		if (id === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo =_.findWhere(todos,{id: id});
+	// var matchedTodo;
+	// todos.forEach(function (todo){
+	// 	if (id === todo.id) {
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -37,8 +39,13 @@ app.get('/todos/:id', function (req, res){
 });
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todoNextId++;
 

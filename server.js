0879ac97d9a9e7cbef2d184b1c35cda56eb3eb2d
Indra Bayu K <indra.b@skyshi.com>
2017-04-27 16:04:04 +0700

    Search

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?compeleted=true
+// GET /todos?compeleted=true&q=house
 app.get('/todos', function (req, res){
 	var queryParams = req.query;
 	var filteredTodo = todos;
@@ -23,6 +23,12 @@ app.get('/todos', function (req, res){
 		filteredTodo = _.where(filteredTodo, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodo = _.where(filteredTodo, {completed: false})
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodo = _.filter(filteredTodo, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	res.json(filteredTodo);
